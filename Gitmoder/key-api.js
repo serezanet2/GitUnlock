d57e8f17a66373e74802eb889f90356653d2cde2
serezanet2 <serezanet2@gmail.com>
2025-06-30 14:55:25 +0300

    Create key-api.js

---
+++ Gitmoder/key-api.js
@@ -0,0 +1,16 @@
+// Проверка ключа через URL-параметр
+const urlParams = new URLSearchParams(window.location.search);
+if (urlParams.has('check')) {
+    const key = urlParams.get('check');
+    const validKeys = JSON.parse(localStorage.getItem('gitmoder_keys') || {};
+    const isValid = validKeys[key] && validKeys[key] > Date.now();
+    
+    // Возвращаем ответ в формате JSON
+    const response = {
+        valid: isValid,
+        key: key,
+        expires: isValid ? validKeys[key] : null
+    };
+    
+    document.write(JSON.stringify(response));
+}
