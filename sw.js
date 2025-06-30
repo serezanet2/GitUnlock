471423317afc463e5fca77e6f8a5748e7ff4216d
serezanet2 <serezanet2@gmail.com>
2025-06-30 14:14:33 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = 'gitunlock-v1';
+const CACHE_NAME = 'gitunlock-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  'icon.png'
+  './',
+  './index.html',
+  './Gitmoder/',
+  './CodeCrush/',
+  './icon.png'
 ];
 
 self.addEventListener('install', event => {
@@ -18,11 +20,30 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
+        // Возвращаем кэшированный ответ, если он есть
         if (response) {
           return response;
         }
-        return fetch(event.request);
-      }
-    )
+        
+        // Иначе загружаем из сети
+        return fetch(event.request)
+          .then(response => {
+            // Проверяем, валидный ли ответ
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            // Клонируем ответ
+            const responseToCache = response.clone();
+            
+            // Кэшируем новый ресурс
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+              
+            return response;
+          });
+      })
   );
 });
